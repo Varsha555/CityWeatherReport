41317ee6029c8460b1a163e38e9f057758bd354d
varsha <Varsha Naveen@DESKTOP-NNSNQ5O>
2020-06-23 16:33:13 +0530

    city weather forecast

---
+++ java/com/c2b/cityweather/main/CityWeatherReport.java
@@ -0,0 +1,15 @@
+package com.c2b.cityweather.main;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityWeatherReport{
+	public static void main(String[] args) {
+		SpringApplication.run(CityWeatherReport.class, args);
+	}
+
+}
+
